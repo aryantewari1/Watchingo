f08db26d64f8a0d21d9de79fc3e662e83097a517
Aryan Tewari <aryantewari308@gmail.com>
2024-09-17 12:43:18 +0530

    added now playing movies api in browse

--- src/components/Browser/Browse.js
+++ src/components/Browser/Browse.js
@@ -1,6 +1,19 @@
 import { Outlet } from "react-router-dom";
 import BrowseHeader from "./BrowseHeader";
+import { API_OPTIONS } from "../../constants/constants";
+import { useEffect } from "react";
 const Browse = () => {
+  const getMovies = async () => {
+    const url =
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+    const data = await fetch(url, API_OPTIONS);
+    const json = await data.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
     <div className="">
       <BrowseHeader />
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,3 +4,11 @@ export const LOGIN_IMG_URL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/dae1f45f-c2c5-4a62-8d58-6e1b0c6b2d8e/6d1fb8a4-5844-42a4-9b01-1c6c128acf19/IN-en-20240827-TRIFECTA-perspective_WEB_c292a608-cdc6-4686-8dc8-405bfcf753af_large.jpg";
 export const LOGO_URL =
   "https://occ-0-3752-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXYofKdCJceEP7pdxcEZ9wt80GsxEyXIbnG_QM8znksNz3JexvRbDLr0_AcNKr2SJtT-MLr1eCOA-e7xlDHsx4Jmmsi5HL8.png?r=1d4";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OWUyNjFkNGExMWY3NjE2NzA2NDBjNDNkMTlhM2I4OSIsIm5iZiI6MTcyNjU1NDMzNi42MDk2MTksInN1YiI6IjY2ZTdkNGI1Mzc2OGE3M2Y4ZDkxYjdiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RC5WBRzEX2HQlvtdTrS61ZGUVfY6sZc_OdKWmX6jFkY",
+  },
+};
